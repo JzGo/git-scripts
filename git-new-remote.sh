511c812e5084367fd53477a926dbc22a20a1223b
Jezreel <jezreel.go@gmail.com>
2019-04-12 15:07:18 -0700

    fix remote name setting and echo

--- git-new-remote.sh
+++ git-new-remote.sh
@@ -7,12 +7,12 @@ REMOTE_URL=$2
 
 # write new remote settings to .git/config
 
-NEW_CONFIG="[remote "$REMOTE_NAME"]
+NEW_CONFIG="[remote \""$REMOTE_NAME\""]
 	url = $REMOTE_URL
 	fetch = +refs/heads/*:refs/remotes/origin/*
 "
 
-echo NEW_CONFIG > .git/config
+echo $NEW_CONFIG > .git/config
 
 echo "
 Your repo has this as a new remote for $REMOTE_NAME: 
