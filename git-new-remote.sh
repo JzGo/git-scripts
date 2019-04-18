9cb23ca98e9ddee3e5d14c67a52fe03741f19fbb
Jezreel <jezreel.go@gmail.com>
2019-04-18 11:54:46 -0700

    add error message

--- git-new-remote.sh
+++ git-new-remote.sh
@@ -5,16 +5,26 @@ REMOTE_NAME=$1
 # take in new remote address
 REMOTE_URL=$2
 
-# write new remote settings to .git/config
+# if arguments are missing, inform the user
+if [ -z "$REMOTE_NAME" ] || [ -z "$REMOTE_URL" ]
+then
+	echo "
+	Argument Missing(s). Please provide a remote name and a remote url in this format:
+	
+	git-new-remote remote-name remote-url
+	"
 
+else
+# create configs when proper arguments are received
+	NEW_CONFIG="\n[remote \""$REMOTE_NAME\""]\n\turl = $REMOTE_URL\n\tfetch = +refs/heads/*:refs/remotes/origin/*
+	"
 
-NEW_CONFIG="[remote \""$REMOTE_NAME\""]\n\turl = $REMOTE_URL\n\tfetch = +refs/heads/*:refs/remotes/origin/*
-"
+	echo -e $NEW_CONFIG >> .git/config
 
-echo -e $NEW_CONFIG >> .git/config
+	echo "
+	Your repo has this as a new remote for $REMOTE_NAME: 
 
-echo "
-Your repo has this as a new remote for $REMOTE_NAME: 
+	$NEW_CONFIG
+	"
 
-$NEW_CONFIG
-"
+fi
